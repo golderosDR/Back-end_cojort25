b88bfd987641a28dd41a22f3023f7757b2abff04
golderosDR <golderos.spr@gmail.com>
2023-08-28 16:20:33 +0200

    probe

--- Task_01/src/test/java/de/ait_tr/services/impl/EventServiceImplTest.java
+++ Task_01/src/test/java/de/ait_tr/services/impl/EventServiceImplTest.java
@@ -1,7 +1,19 @@
 package de.ait_tr.services.impl;
 
+import de.ait_tr.models.Event;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EventServiceImplTest {
+    private static final String EXIST_USER_EMAIL = "user@gmail.com";
+    private static final String NOT_EXIST_USER_EMAIL = "marsel@gmail.com";
+    private static final String DEFAULT_PASSWORD = "qwerty007";
+    private static final Event NOT_EXIST_USER = new (NOT_EXIST_USER_EMAIL, DEFAULT_PASSWORD);
+    private static final Event EXIST_USER = new User(EXIST_USER_EMAIL, DEFAULT_PASSWORD);
+
+
+    private UsersServiceImpl usersService; // объект, который мы будем тестировать
+
+    private UsersRepository usersRepository;
 
 }
